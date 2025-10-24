221fc6ee0656592c923cbba6273a4e5efdca7632
MathisGD <mathis@morpho.xyz>
2025-10-24 12:04:40 +0200

    fix: dutch computation

--- src/libraries/ConstantsLib.sol
+++ src/libraries/ConstantsLib.sol
@@ -5,4 +5,4 @@ pragma solidity ^0.8.0;
 uint256 constant WAD = 1e18;
 uint256 constant ORACLE_PRICE_SCALE = 1e36;
 uint256 constant MAX_LIF = 1.15e18;
-uint256 constant DUTCH_SPEED = MAX_LIF / 15 minutes;
+uint256 constant DUTCH_SPEED = (MAX_LIF - WAD) / 15 minutes;
