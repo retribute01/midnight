33b7186f7b60ea2f9925aa22a4723ac4fc491085
Quentin Garchery <garchery.quentin@gmail.com>
2026-01-30 19:17:17 +0100

    refactor: remove idToObligationContract

--- src/libraries/IdLib.sol
+++ src/libraries/IdLib.sol
@@ -12,12 +12,9 @@ library IdLib {
         return keccak256(creationCode);
     }
 
-    function idToObligationContract(address morphoV2, bytes32 id) internal pure returns (address) {
-        return address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), morphoV2, bytes32(0), id)))));
-    }
-
     function idToObligation(address morphoV2, bytes32 id) internal view returns (Obligation memory) {
-        address obligationContract = idToObligationContract(morphoV2, id);
-        return abi.decode(obligationContract.code, (Obligation));
+        address create2Address =
+            address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), morphoV2, bytes32(0), id)))));
+        return abi.decode(create2Address.code, (Obligation));
     }
 }
