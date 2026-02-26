92f41ed54f836718203b66189027b870e6528277
MathisGD <74971347+MathisGD@users.noreply.github.com>
2026-02-26 10:05:41 +0100

    docs fix
    Co-authored-by: Quentin Garchery <garchery.quentin@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/MorphoV2.sol
+++ src/MorphoV2.sol
@@ -628,7 +628,7 @@ contract MorphoV2 is IMorphoV2 {
         return tentativeSigner;
     }
 
-    /// @dev 50bps for ttm=360 days, scaled linearly. For post maturity, 1.4bps.
+    /// @dev 50bps for ttm=360 days, scaled linearly. For post maturity, 0.14bps.
     function maxTradingFee(uint256 index) public pure returns (uint256) {
         return [0.000014e18, 0.000014e18, 0.000098e18, 0.000417e18, 0.00125e18, 0.0025e18, 0.005e18][index];
     }
