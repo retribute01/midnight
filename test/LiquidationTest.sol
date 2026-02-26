60d5aa97ec16778a3cd923c2189e896793795139
Quentin Garchery <garchery.quentin@gmail.com>
2026-02-26 22:40:57 +0100

    show that there is a path to clean the position

--- test/LiquidationTest.sol
+++ test/LiquidationTest.sol
@@ -452,6 +452,60 @@ contract LiquidationTest is BaseTest {
         assertEq(morphoV2.debtOf(id, borrower), 0);
     }
 
+    /// @dev With RCF deactivated, liquidation can always end by fully repaying debt or fully seizing collateral.
+    function testLiquidateFullyRepayOrFullySeizeWhenRcfDeactivated(
+        uint256 units,
+        uint256 collateral1,
+        uint256 collateral2
+    ) public {
+        collateral1 = bound(collateral1, 1, MAX_TEST_AMOUNT);
+        collateral2 = bound(collateral2, 1, MAX_TEST_AMOUNT);
+
+        // Deactivate RCF.
+        obligation.rcfThreshold = type(uint256).max;
+        id = toId(obligation);
+
+        // Price is 1 initially, assume liquidatable but no bad debt.
+        uint256 maxDebt = collateral1.mulDivDown(obligation.collaterals[0].lltv, WAD)
+            + collateral2.mulDivDown(obligation.collaterals[1].lltv, WAD);
+        uint256 repayableDebt = collateral1.mulDivDown(WAD, MAX_LIF) + collateral2.mulDivDown(WAD, MAX_LIF);
+        units = bound(units, maxDebt, repayableDebt);
+        vm.assume(units > maxDebt);
+
+        // Write the debt first, supply collateral later, so that the activated collaterals are not overwritten.
+        uint256 mappingSlot = 1;
+        bytes32 intermediateSlot = keccak256(abi.encode(id, mappingSlot));
+        bytes32 borrowerSlot = keccak256(abi.encode(borrower, intermediateSlot));
+        vm.store(address(morphoV2), borrowerSlot, bytes32(units));
+
+        assertEq(morphoV2.debtOf(id, borrower), units, "debt");
+
+        // Collateralize with both collaterals.
+        deal(obligation.collaterals[0].token, address(this), collateral1);
+        ERC20(obligation.collaterals[0].token).approve(address(morphoV2), collateral1);
+        morphoV2.supplyCollateral(obligation, 0, collateral1, borrower);
+
+        deal(obligation.collaterals[1].token, address(this), collateral2);
+        ERC20(obligation.collaterals[1].token).approve(address(morphoV2), collateral2);
+        morphoV2.supplyCollateral(obligation, 1, collateral2, borrower);
+
+        // Check that the position is unhealthy but has no bad debt.
+        // If it had bad debt, this can be taken into account separately.
+        assertEq(_badDebt(), 0, "no bad debt");
+        assertFalse(morphoV2.isHealthy(obligation, id, borrower), "position should be unhealthy");
+
+        uint256 collateralNeededToRepayAll = units.mulDivDown(MAX_LIF, WAD);
+        if (collateralNeededToRepayAll <= collateral1) {
+            morphoV2.liquidate(obligation, 0, 0, units, borrower, "");
+        } else {
+            morphoV2.liquidate(obligation, 0, collateral1, 0, borrower, "");
+        }
+
+        uint256 debtAfter = morphoV2.debtOf(id, borrower);
+        uint256 collateralAfter = morphoV2.collateralOf(id, borrower, 0);
+        assertTrue(debtAfter == 0 || collateralAfter == 0, "either debt repaid or collateral seized");
+    }
+
     /// @dev Recovery close factor with two collaterals contributing to maxDebt.
     /// Drops price of the lower-lltv collateral to make position unhealthy, then liquidates it.
     function testRecoveryCloseFactorMultipleCollaterals(uint256 units) public {
