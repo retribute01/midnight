b95fbb76d44f58b7c759faa9cbf58cfcc52a23c0
Adrien Husson <adhusson@gmail.com>
2026-03-03 17:20:08 +0100

    test: fix

--- test/SignatureTest.sol
+++ test/SignatureTest.sol
@@ -10,8 +10,11 @@ import {Test} from "../lib/forge-std/src/Test.sol";
 contract SignatureTest is Test, Midnight {
     function testSigner(bytes32 root, uint256 privateKey) public view {
         privateKey = boundPrivateKey(privateKey);
+        bytes32 domainSeparator = vm.eip712HashStruct(
+            "EIP712Domain(uint256 chainId,address verifyingContract)", abi.encode(block.chainid, address(this))
+        );
         bytes32 structHash = keccak256(abi.encode(ROOT_TYPEHASH, root));
-        bytes32 digest = keccak256(bytes.concat("\x19\x01", domainSeparator(), structHash));
+        bytes32 digest = keccak256(bytes.concat("\x19\x01", domainSeparator, structHash));
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
         assertEq(signer(root, Signature({v: v, r: r, s: s})), vm.addr(privateKey));
     }
@@ -21,16 +24,5 @@ contract SignatureTest is Test, Midnight {
             "EIP712Domain(uint256 chainId,address verifyingContract)", abi.encode(block.chainid, address(this))
         );
         assertEq(domainSeparator(), expectedDomainSeparator);
-    }
-
-    function testSigner(bytes32 root, uint256 privateKey) public view {
-        privateKey = boundPrivateKey(privateKey);
-        bytes32 expectedDomainSeparator = vm.eip712HashStruct(
-            "EIP712Domain(uint256 chainId,address verifyingContract)", abi.encode(block.chainid, address(this))
-        );
-        bytes32 expectedStructHash = vm.eip712HashStruct("Root(bytes32 root)", abi.encode(root));
-        bytes32 digest = keccak256(bytes.concat("\x19\x01", expectedDomainSeparator, refStructHash));
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, expectedDigest);
-        assertEq(signer(root, Signature({v: v, r: r, s: s})), vm.addr(privateKey));
     }
 }
