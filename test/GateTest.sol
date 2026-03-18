1788941cf289aec5fabd8ff559d90e94486d511e
peyha <peyha.nicole@outlook.fr>
2026-03-18 10:50:28 +0100

    test: adapt gate test

--- test/GateTest.sol
+++ test/GateTest.sol
@@ -2,10 +2,11 @@
 // Copyright (c) 2025 Morpho Association
 pragma solidity ^0.8.0;
 
-import {Obligation, Offer, Collateral} from "../src/interfaces/IMorphoV2.sol";
+import {Obligation, Offer, Collateral} from "../src/interfaces/IMidnight.sol";
 import {ILenderGate, IBorrowerGate, ILiquidatorGate} from "../src/interfaces/IGate.sol";
+import {LIQUIDATION_CURSOR_LOW} from "../src/libraries/ConstantsLib.sol";
 import {UtilsLib} from "../src/libraries/UtilsLib.sol";
-import {TICK_RANGE} from "../src/libraries/TickLib.sol";
+import {MAX_TICK} from "../src/libraries/TickLib.sol";
 import {BaseTest, MAX_TEST_AMOUNT} from "./BaseTest.sol";
 import {Oracle} from "./helpers/Oracle.sol";
 
@@ -48,14 +49,28 @@ contract GateTest is BaseTest {
         obligation.loanToken = address(loanToken);
         obligation.maturity = block.timestamp + 100;
         obligation.collaterals
-            .push(Collateral({token: address(collateralToken1), lltv: 0.75e18, oracle: address(oracle1)}));
+            .push(
+                Collateral({
+                    token: address(collateralToken1),
+                    lltv: 0.75e18,
+                    oracle: address(oracle1),
+                    maxLif: maxLif(0.75e18, LIQUIDATION_CURSOR_LOW)
+                })
+            );
         obligation.collaterals = sortCollaterals(obligation.collaterals);
 
         // Gated obligation (same terms, but with gates).
         gatedObligation.loanToken = address(loanToken);
         gatedObligation.maturity = block.timestamp + 100;
         gatedObligation.collaterals
-            .push(Collateral({token: address(collateralToken1), lltv: 0.75e18, oracle: address(oracle1)}));
+            .push(
+                Collateral({
+                    token: address(collateralToken1),
+                    lltv: 0.75e18,
+                    oracle: address(oracle1),
+                    maxLif: maxLif(0.75e18, LIQUIDATION_CURSOR_LOW)
+                })
+            );
         gatedObligation.collaterals = sortCollaterals(gatedObligation.collaterals);
         gatedObligation.lenderGate = address(gate);
         gatedObligation.borrowerGate = address(gate);
@@ -65,84 +80,84 @@ contract GateTest is BaseTest {
 
         lenderOffer.buy = true;
         lenderOffer.maker = lender;
-        lenderOffer.assets = type(uint256).max;
+        lenderOffer.obligationUnits = type(uint256).max;
         lenderOffer.obligation = gatedObligation;
         lenderOffer.expiry = block.timestamp + 200;
-        lenderOffer.tick = TICK_RANGE;
+        lenderOffer.tick = MAX_TICK;
 
         borrowerOffer.buy = false;
         borrowerOffer.maker = borrower;
         borrowerOffer.receiverIfMakerIsSeller = borrower;
-        borrowerOffer.assets = type(uint256).max;
+        borrowerOffer.obligationUnits = type(uint256).max;
         borrowerOffer.obligation = gatedObligation;
         borrowerOffer.expiry = block.timestamp + 200;
-        borrowerOffer.tick = TICK_RANGE;
+        borrowerOffer.tick = MAX_TICK;
 
         deal(address(loanToken), lender, type(uint256).max);
     }
 
     // --- Lender gate tests ---
 
-    function testLenderGateBlocksNonWhitelistedLender(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
-        collateralize(gatedObligation, borrower, buyerAssets);
+    function testLenderGateBlocksNonWhitelistedLender(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
+        collateralize(gatedObligation, borrower, obligationUnits);
 
         // Borrower is whitelisted, lender is not.
         gate.setWhitelisted(borrower, true);
 
-        vm.expectRevert("lender gated from lending");
-        take(buyerAssets, 0, 0, 0, lender, borrowerOffer);
+        vm.expectRevert("buyer gated from lending");
+        take(obligationUnits, lender, borrowerOffer);
     }
 
-    function testLenderGateAllowsWhitelistedLender(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
-        collateralize(gatedObligation, borrower, buyerAssets);
+    function testLenderGateAllowsWhitelistedLender(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
+        collateralize(gatedObligation, borrower, obligationUnits);
 
         gate.setWhitelisted(lender, true);
         gate.setWhitelisted(borrower, true);
 
-        take(buyerAssets, 0, 0, 0, lender, borrowerOffer);
+        take(obligationUnits, lender, borrowerOffer);
 
-        assertGt(morphoV2.sharesOf(gatedId, lender), 0, "lender should have shares");
+        assertGt(midnight.creditOf(gatedId, lender), 0, "lender should have credit");
     }
 
     // --- Borrower gate tests ---
 
-    function testBorrowerGateBlocksNonWhitelistedBorrower(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
-        collateralize(gatedObligation, borrower, buyerAssets);
+    function testBorrowerGateBlocksNonWhitelistedBorrower(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
+        collateralize(gatedObligation, borrower, obligationUnits);
 
         // Lender is whitelisted, borrower is not.
         gate.setWhitelisted(lender, true);
 
-        vm.expectRevert("borrower gated from borrowing");
-        take(0, 0, buyerAssets, 0, borrower, lenderOffer);
+        vm.expectRevert("seller gated from borrowing");
+        take(obligationUnits, borrower, lenderOffer);
     }
 
-    function testBorrowerGateAllowsWhitelistedBorrower(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
-        collateralize(gatedObligation, borrower, buyerAssets);
+    function testBorrowerGateAllowsWhitelistedBorrower(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
+        collateralize(gatedObligation, borrower, obligationUnits);
 
         gate.setWhitelisted(lender, true);
         gate.setWhitelisted(borrower, true);
 
-        take(0, 0, buyerAssets, 0, borrower, lenderOffer);
+        take(obligationUnits, borrower, lenderOffer);
 
-        assertGt(morphoV2.debtOf(gatedId, borrower), 0, "borrower should have debt");
+        assertGt(midnight.debtOf(gatedId, borrower), 0, "borrower should have debt");
     }
 
     // --- No gate check on exit paths ---
 
-    function testNoLenderGateCheckWhenBorrowerIsExitingBorrower(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
+    function testNoLenderGateCheckWhenBorrowerIsExitingBorrower(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
         gate.setWhitelisted(lender, true);
         gate.setWhitelisted(borrower, true);
         gate.setWhitelisted(otherBorrower, true);
 
         // Setup: lender lends, borrower borrows.
-        deal(address(loanToken), lender, buyerAssets);
-        collateralize(gatedObligation, borrower, buyerAssets);
-        take(buyerAssets, 0, 0, 0, lender, borrowerOffer);
+        deal(address(loanToken), lender, obligationUnits);
+        collateralize(gatedObligation, borrower, obligationUnits);
+        take(obligationUnits, lender, borrowerOffer);
 
         // Now borrower (who has debt) takes borrowerOffer as buyer to exit debt.
         // Buyer is exiting borrower + seller enters as borrower.
@@ -151,36 +166,36 @@ contract GateTest is BaseTest {
         otherBorrowerOffer.buy = false;
         otherBorrowerOffer.maker = otherBorrower;
         otherBorrowerOffer.receiverIfMakerIsSeller = otherBorrower;
-        otherBorrowerOffer.assets = type(uint256).max;
+        otherBorrowerOffer.obligationUnits = type(uint256).max;
         otherBorrowerOffer.obligation = gatedObligation;
         otherBorrowerOffer.expiry = block.timestamp + 200;
-        otherBorrowerOffer.tick = TICK_RANGE;
+        otherBorrowerOffer.tick = MAX_TICK;
 
-        collateralize(gatedObligation, otherBorrower, buyerAssets);
+        collateralize(gatedObligation, otherBorrower, obligationUnits);
 
         // Remove lender whitelist.
         gate.setWhitelisted(lender, false);
 
         // borrower takes this offer as buyer (exiting debt), otherBorrower enters as seller (new borrower).
-        take(0, 0, buyerAssets, 0, borrower, otherBorrowerOffer);
+        take(obligationUnits, borrower, otherBorrowerOffer);
 
-        assertEq(morphoV2.debtOf(gatedId, borrower), 0, "borrower should have exited debt");
+        assertEq(midnight.debtOf(gatedId, borrower), 0, "borrower should have exited debt");
     }
 
-    function testNoGateCheckWhenBothExit(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
+    function testNoGateCheckWhenBothExit(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT / 2);
         gate.setWhitelisted(lender, true);
         gate.setWhitelisted(borrower, true);
 
         // Setup: lender lends, borrower borrows.
-        deal(address(loanToken), lender, buyerAssets);
-        collateralize(gatedObligation, borrower, buyerAssets);
-        take(buyerAssets, 0, 0, 0, lender, borrowerOffer);
+        deal(address(loanToken), lender, obligationUnits);
+        collateralize(gatedObligation, borrower, obligationUnits);
+        take(obligationUnits, lender, borrowerOffer);
 
         // Borrower repays to make funds withdrawable.
-        deal(address(loanToken), borrower, buyerAssets);
+        deal(address(loanToken), borrower, obligationUnits);
         vm.prank(borrower);
-        morphoV2.repay(gatedObligation, buyerAssets, borrower);
+        midnight.repay(gatedObligation, obligationUnits, borrower);
 
         // Remove all whitelisting. Path 4 (both exit) should not check gates.
         gate.setWhitelisted(lender, false);
@@ -195,18 +210,18 @@ contract GateTest is BaseTest {
         gate.setWhitelisted(otherLender, true);
         gate.setWhitelisted(otherBorrower, true);
 
-        deal(address(loanToken), otherLender, buyerAssets);
-        collateralize(gatedObligation, otherBorrower, buyerAssets);
+        deal(address(loanToken), otherLender, obligationUnits);
+        collateralize(gatedObligation, otherBorrower, obligationUnits);
 
         Offer memory otherLenderOffer;
         otherLenderOffer.buy = true;
         otherLenderOffer.maker = otherLender;
-        otherLenderOffer.assets = type(uint256).max;
+        otherLenderOffer.obligationUnits = type(uint256).max;
         otherLenderOffer.obligation = gatedObligation;
         otherLenderOffer.expiry = block.timestamp + 200;
-        otherLenderOffer.tick = TICK_RANGE;
+        otherLenderOffer.tick = MAX_TICK;
 
-        take(0, 0, buyerAssets, 0, otherBorrower, otherLenderOffer);
+        take(obligationUnits, otherBorrower, otherLenderOffer);
 
         // Now otherBorrower has debt, otherLender has shares. Path 4: both exit.
         // otherBorrower (buyer, has debt) buys from otherLender (seller, has shares).
@@ -214,83 +229,77 @@ contract GateTest is BaseTest {
         exitOffer.buy = false;
         exitOffer.maker = otherLender;
         exitOffer.receiverIfMakerIsSeller = otherLender;
-        exitOffer.assets = type(uint256).max;
+        exitOffer.obligationUnits = type(uint256).max;
         exitOffer.obligation = gatedObligation;
         exitOffer.expiry = block.timestamp + 200;
-        exitOffer.tick = TICK_RANGE;
+        exitOffer.tick = MAX_TICK;
 
-        // Remove all whitelisting.
-        gate.setWhitelisted(otherLender, false);
-        gate.setWhitelisted(otherBorrower, false);
+        // Both parties need to remain whitelisted since gates are always checked.
+        deal(address(loanToken), otherBorrower, obligationUnits);
+        take(obligationUnits, otherBorrower, exitOffer);
 
-        // Should succeed: neither gate is checked because both are exiting.
-        deal(address(loanToken), otherBorrower, buyerAssets);
-        take(0, 0, buyerAssets, 0, otherBorrower, exitOffer);
-
-        assertEq(morphoV2.debtOf(gatedId, otherBorrower), 0, "otherBorrower should have exited");
-        assertEq(morphoV2.sharesOf(gatedId, otherLender), 0, "otherLender should have exited");
+        assertEq(midnight.debtOf(gatedId, otherBorrower), 0, "otherBorrower should have exited");
     }
 
     // --- Liquidator gate tests ---
 
-    function testLiquidatorGateBlocksNonWhitelistedLiquidator(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
+    function testLiquidatorGateBlocksNonWhitelistedLiquidator(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
         gate.setWhitelisted(lender, true);
         gate.setWhitelisted(borrower, true);
 
-        deal(address(loanToken), lender, buyerAssets);
-        collateralize(gatedObligation, borrower, buyerAssets);
-        take(buyerAssets, 0, 0, 0, lender, borrowerOffer);
+        deal(address(loanToken), lender, obligationUnits);
+        collateralize(gatedObligation, borrower, obligationUnits);
+        take(obligationUnits, lender, borrowerOffer);
 
         // Make position liquidatable by dropping oracle price.
         Oracle(gatedObligation.collaterals[0].oracle).setPrice(0);
 
         // Liquidator is NOT whitelisted.
-        deal(address(loanToken), liquidator, buyerAssets);
+        deal(address(loanToken), liquidator, obligationUnits);
         vm.prank(liquidator);
         vm.expectRevert("liquidator gated from liquidating");
-        morphoV2.liquidate(gatedObligation, 0, 0, 0, borrower, "");
+        midnight.liquidate(gatedObligation, 0, 0, 0, borrower, "");
     }
 
-    function testLiquidatorGateAllowsWhitelistedLiquidator(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
+    function testLiquidatorGateAllowsWhitelistedLiquidator(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
         gate.setWhitelisted(lender, true);
         gate.setWhitelisted(borrower, true);
         gate.setWhitelisted(liquidator, true);
 
-        deal(address(loanToken), lender, buyerAssets);
-        collateralize(gatedObligation, borrower, buyerAssets);
-        take(buyerAssets, 0, 0, 0, lender, borrowerOffer);
+        deal(address(loanToken), lender, obligationUnits);
+        collateralize(gatedObligation, borrower, obligationUnits);
+        take(obligationUnits, lender, borrowerOffer);
 
         // Make position liquidatable.
         Oracle(gatedObligation.collaterals[0].oracle).setPrice(0);
 
-        deal(address(loanToken), liquidator, buyerAssets);
+        deal(address(loanToken), liquidator, obligationUnits);
         vm.prank(liquidator);
-        morphoV2.liquidate(gatedObligation, 0, 0, 0, borrower, "");
+        midnight.liquidate(gatedObligation, 0, 0, 0, borrower, "");
     }
 
     // --- Default (no gate) tests ---
 
-    function testNoGateMeansUnrestricted(uint256 buyerAssets) public {
-        buyerAssets = bound(buyerAssets, 1, MAX_TEST_AMOUNT);
-        deal(address(loanToken), lender, buyerAssets);
-        collateralize(obligation, borrower, buyerAssets);
+    function testNoGateMeansUnrestricted(uint256 obligationUnits) public {
+        obligationUnits = bound(obligationUnits, 1, MAX_TEST_AMOUNT * 3 / 4);
+        deal(address(loanToken), lender, obligationUnits);
+        collateralize(obligation, borrower, obligationUnits);
 
         Offer memory ungatedLenderOffer;
         ungatedLenderOffer.buy = true;
         ungatedLenderOffer.maker = lender;
-        ungatedLenderOffer.assets = type(uint256).max;
+        ungatedLenderOffer.obligationUnits = type(uint256).max;
         ungatedLenderOffer.obligation = obligation;
         ungatedLenderOffer.expiry = block.timestamp + 200;
-        ungatedLenderOffer.tick = TICK_RANGE;
+        ungatedLenderOffer.tick = MAX_TICK;
 
         // No gates set — anyone can participate.
-        take(0, 0, buyerAssets, 0, borrower, ungatedLenderOffer);
+        take(obligationUnits, borrower, ungatedLenderOffer);
 
         bytes32 ungatedId = toId(obligation);
-        assertGt(morphoV2.sharesOf(ungatedId, lender), 0);
-        assertGt(morphoV2.debtOf(ungatedId, borrower), 0);
+        assertGt(midnight.debtOf(ungatedId, borrower), 0);
     }
 
     // --- Obligation identity tests ---
@@ -305,24 +314,24 @@ contract GateTest is BaseTest {
     function testCanLendViewFunction() public {
         gate.setWhitelisted(lender, true);
 
-        assertTrue(morphoV2.canLend(gatedObligation, lender), "whitelisted should pass");
-        assertFalse(morphoV2.canLend(gatedObligation, borrower), "non-whitelisted should fail");
-        assertTrue(morphoV2.canLend(obligation, borrower), "no gate should always pass");
+        assertTrue(midnight.canLend(gatedObligation, lender), "whitelisted should pass");
+        assertFalse(midnight.canLend(gatedObligation, borrower), "non-whitelisted should fail");
+        assertTrue(midnight.canLend(obligation, borrower), "no gate should always pass");
     }
 
     function testCanBorrowViewFunction() public {
         gate.setWhitelisted(borrower, true);
 
-        assertTrue(morphoV2.canBorrow(gatedObligation, borrower), "whitelisted should pass");
-        assertFalse(morphoV2.canBorrow(gatedObligation, lender), "non-whitelisted should fail");
-        assertTrue(morphoV2.canBorrow(obligation, lender), "no gate should always pass");
+        assertTrue(midnight.canBorrow(gatedObligation, borrower), "whitelisted should pass");
+        assertFalse(midnight.canBorrow(gatedObligation, lender), "non-whitelisted should fail");
+        assertTrue(midnight.canBorrow(obligation, lender), "no gate should always pass");
     }
 
     function testCanLiquidateViewFunction() public {
         gate.setWhitelisted(liquidator, true);
 
-        assertTrue(morphoV2.canLiquidate(gatedObligation, liquidator), "whitelisted should pass");
-        assertFalse(morphoV2.canLiquidate(gatedObligation, lender), "non-whitelisted should fail");
-        assertTrue(morphoV2.canLiquidate(obligation, lender), "no gate should always pass");
+        assertTrue(midnight.canLiquidate(gatedObligation, liquidator), "whitelisted should pass");
+        assertFalse(midnight.canLiquidate(gatedObligation, lender), "non-whitelisted should fail");
+        assertTrue(midnight.canLiquidate(obligation, lender), "no gate should always pass");
     }
 }
