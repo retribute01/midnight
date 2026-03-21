32859de2cf0b0e6e8f41229587ff32f053dc9ce0
MathisGD <mathis@morpho.xyz>
2026-03-21 16:31:32 +0100

    fix merge

--- src/Midnight.sol
+++ src/Midnight.sol
@@ -216,8 +216,6 @@ contract Midnight is IMidnight {
         require(UtilsLib.isLeaf(root, keccak256(abi.encode(offer)), proof), "invalid proof");
         require(offer.session == session[offer.maker], "invalid session");
         bytes32 id = touchObligation(offer.obligation);
-        _updatePosition(offer.obligation, id, offer.maker);
-        _updatePosition(offer.obligation, id, taker);
         ObligationState storage _obligationState = obligationState[id];
 
         (
@@ -262,12 +260,9 @@ contract Midnight is IMidnight {
         Position storage buyerPos = position[id][buyer];
         Position storage sellerPos = position[id][seller];
 
-        if (buyerPos.credit > 0 || units > buyerPos.debt) slash(id, buyer);
-        if (buyerPos.pendingFee > 0 || units > buyerPos.debt) {
-            accrueContinuousFee(offer.obligation, id, buyer);
-        }
-        if (sellerPos.credit > 0) slash(id, seller);
-        if (sellerPos.pendingFee > 0) accrueContinuousFee(offer.obligation, id, seller);
+        if (buyerPos.credit > 0 || units > buyerPos.debt) _updatePosition(offer.obligation, id, buyer);
+        if (sellerPos.credit > 0) _updatePosition(offer.obligation, id, seller);
+
         uint256 buyerCreditIncrease = UtilsLib.zeroFloorSub(units, buyerPos.debt);
         uint256 sellerCreditDecrease = UtilsLib.min(units, sellerPos.credit);
         buyerPos.debt -= UtilsLib.toUint128(units - buyerCreditIncrease);
